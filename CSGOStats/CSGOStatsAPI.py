f890a53efcf3be8fe9e7afb772a525ea4fab2208
CorentinMre <corentin.marie@laposte.net>
2022-06-06 09:43:47 +0200

    CSGOStatsAPI

--- CSGOStats/CSGOStatsAPI.py
+++ CSGOStats/CSGOStatsAPI.py
@@ -1,4 +1,5 @@
 
+from unicodedata import category
 import requests
 from bs4 import BeautifulSoup
 
@@ -26,27 +27,72 @@ class CSGOStats:
             soup_object = BeautifulSoup(site_resp.text, "lxml")
             self.steam_id = soup_object.find_all("a")[0].get("href").split("/")[-1][:-2]
 
-            stats_url = f"https://tracker.gg/csgo/profile/steam/{self.steam_id}/overview"
-            site_resp3 = requests.get(stats_url, headers=headers)
-            soup_object3 = BeautifulSoup(site_resp3.text, "lxml")
+            #########OVERVIEW#########
+            stats_url_overview = f"https://tracker.gg/csgo/profile/steam/{self.steam_id}/overview"
+            site_resp_overview = requests.get(stats_url_overview, headers=headers)
+            soup_object_overview = BeautifulSoup(site_resp_overview.text, "lxml")
 
 
-            self.informations = {"avatar" : soup_object3.find_all("img", {"class":"ph-avatar__image"})[0].get("src"),
-                                "kd" : soup_object3.find_all("span", {"class":"value"})[0].text,
-                                "headshot" : soup_object3.find_all("span", {"class":"value"})[1].text,
-                                "win" : soup_object3.find_all("span", {"class":"value"})[2].text,
-                                "mvp" : soup_object3.find_all("span", {"class":"value"})[3].text,
-                                "kill" : soup_object3.find_all("span", {"class":"value"})[4].text,
-                                "deaths" : soup_object3.find_all("span", {"class":"value"})[5].text,
-                                "headshot_nb" : soup_object3.find_all("span", {"class":"value"})[6].text,
-                                "win_nb" : soup_object3.find_all("span", {"class":"value"})[7].text,
-                                "losses_nb" : soup_object3.find_all("span", {"class":"value"})[8].text,
-                                "score" : soup_object3.find_all("span", {"class":"value"})[9].text,
-                                "damage" : soup_object3.find_all("span", {"class":"value"})[10].text,
-                                "shotsAccuracy" : soup_object3.find_all("span", {"class":"value"})[11].text,
-                                "bombsPlanted" : soup_object3.find_all("span", {"class":"value"})[12].text,
-                                "bombsDefused" : soup_object3.find_all("span", {"class":"value"})[13].text,
-                                "moneyEarned" : soup_object3.find_all("span", {"class":"value"})[14].text,
-                                "hostageRescued" : soup_object3.find_all("span", {"class":"value"})[15].text}
+            self.informations_overview = {"avatar" : soup_object_overview.find_all("img", {"class":"ph-avatar__image"})[0].get("src"),
+                                "kd" : soup_object_overview.find_all("span", {"class":"value"})[0].text,
+                                "headshot" : soup_object_overview.find_all("span", {"class":"value"})[1].text,
+                                "win" : soup_object_overview.find_all("span", {"class":"value"})[2].text,
+                                "mvp" : soup_object_overview.find_all("span", {"class":"value"})[3].text,
+                                "kill" : soup_object_overview.find_all("span", {"class":"value"})[4].text,
+                                "deaths" : soup_object_overview.find_all("span", {"class":"value"})[5].text,
+                                "headshot_nb" : soup_object_overview.find_all("span", {"class":"value"})[6].text,
+                                "win_nb" : soup_object_overview.find_all("span", {"class":"value"})[7].text,
+                                "losses_nb" : soup_object_overview.find_all("span", {"class":"value"})[8].text,
+                                "score" : soup_object_overview.find_all("span", {"class":"value"})[9].text,
+                                "damage" : soup_object_overview.find_all("span", {"class":"value"})[10].text,
+                                "shotsAccuracy" : soup_object_overview.find_all("span", {"class":"value"})[11].text,
+                                "bombsPlanted" : soup_object_overview.find_all("span", {"class":"value"})[12].text,
+                                "bombsDefused" : soup_object_overview.find_all("span", {"class":"value"})[13].text,
+                                "moneyEarned" : soup_object_overview.find_all("span", {"class":"value"})[14].text,
+                                "hostageRescued" : soup_object_overview.find_all("span", {"class":"value"})[15].text}
+
+            #############Weapons#############
+            stats_url_weapons = f"https://tracker.gg/csgo/profile/steam/{self.steam_id}/weapons"
+            site_resp_weapons = requests.get(stats_url_weapons, headers=headers)
+            soup_object_weapons = BeautifulSoup(site_resp_weapons.text, "lxml")
+            nb_weapons = len(soup_object_weapons.find_all("tr"))
+            self.informations_weapons = {}
+            for i in range(nb_weapons -1):
+
+                temp_weapons = {}
+                temp_weapons["Icons"] = soup_object_weapons.find_all("tr")[i+1].find_all("img")[0].get("src")
+                temp_weapons["Weapon"] = soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[0].text
+                temp_weapons["KillsShots"] = soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[1].text
+                temp_weapons["FiredShots"] = soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[2].text
+                temp_weapons["HitShots"] = soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[3].text
+                temp_weapons["Accuracy"] = soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[4].text
+                self.informations_weapons[soup_object_weapons.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[0].text] = temp_weapons
+
+            ###############MAPS##############
+            stats_url_maps = f"https://tracker.gg/csgo/profile/steam/{self.steam_id}/maps"
+            site_resp_maps = requests.get(stats_url_maps, headers=headers)
+            soup_object_maps = BeautifulSoup(site_resp_maps.text, "lxml")
+            nb_maps = len(soup_object_maps.find_all("tr"))
+            self.informations_maps = {}
+            for i in range(nb_maps -1):
+
+                temp_maps = {}
+                temp_maps["Icons"] = soup_object_maps.find_all("tr")[i+1].find_all("img")[0].get("src")
+                temp_maps["Map"] = soup_object_maps.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[0].text
+                temp_maps["Wins"] = soup_object_maps.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[1].text
+                temp_maps["Rounds"] = soup_object_maps.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[2].text
+                self.informations_maps[soup_object_maps.find_all("tr")[i+1].find_all("span", {"class":"segment-used__tp-name"})[0].text] = temp_maps
+
+        
         except:
             self.no_error = False
+
+
+if __name__ == "__main__":
+    test = CSGOStats("footsx")
+    if test.no_error:
+        print(test.informations_overview)
+        print(test.informations_weapons)
+        print(test.informations_maps)
+    else:
+        print("No information about this person")
--- example.py
+++ example.py
@@ -2,9 +2,11 @@
 from CSGOStats import CSGOStats
 
 
-test = CSGOStats("Ritchi92")
+test = CSGOStats("footsx")
 
 if test.no_error:
-    print(test.informations)
+        print(test.informations_overview)
+        print(test.informations_weapons)
+        print(test.informations_maps)
 else:
     print("No information about this person")
